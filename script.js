db833fc1fe705b5d087a41d654ad8c932550620b
Anandh Kumar <accetanandhmca@gmail.com>
2023-07-28 01:20:07 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,43 +1,18 @@
+const container = document.createElement("div");
+container.setAttribute("class", "container");
 
+const heading = document.createElement("h1");
+heading.setAttribute("id","title");
+heading.setAttribute("class","text-center");
+heading.innerHTML = "Rest Countries & Weather Using Fetch API"
+document.body.appendChild(heading);
 
-var containerDiv = createDivWithClass('container');
-var containerRow = createDivWithClassAndId('row','main-div');
-var title1 = createH1WithId('title','RestCountries and Weather using fetch API');
+const row = document.createElement("div");
+row.setAttribute("class", "row");
 
-var descriptionPara = document.createElement('p');
-descriptionPara.id = 'description';
-descriptionPara.innerText = 'Implementation of the Rest countries and Open Weather map APIs using fetch()API';
-document.body.append(descriptionPara);
-
-containerDiv.appendChild(containerRow);
-containerRow.appendChild(title1);
-containerRow.appendChild(descriptionPara);
-
-function createDivWithClass(classValue) {
-    var divElement = document.createElement('div');
-    divElement.className=(classValue);
-    document.body.append(divElement);
-    return divElement;
-}
-
-function createDivWithClassAndId(className,idName) {
-    var divElement = document.createElement('div');
-    divElement.className=(className);
-    divElement.id=(idName);
-    document.body.append(divElement);
-    return divElement;
-}
-
-function createH1WithId(idName,content){
-    var h1Div = document.createElement('h1');
-    h1Div.id = (idName);
-    h1Div.innerText = (content);
-    document.body.append(h1Div);
-    return h1Div;
-}
-
-//getting the div element in which all html elements needs to be inserted
-let mainDiv=document.querySelector("#main-div");
+document.body.append(container);
+container.appendChild(heading);
+container.appendChild(row);
 
 //creating an async function for fetching rest countries API
 const displayRestCountries = async ()=>{
@@ -46,26 +21,25 @@ const displayRestCountries = async ()=>{
     return data; //returning json data of API
 }
 
-displayRestCountries()
-.then(data=>{ 
-    //creating elements to store rest countries data
-    mainDiv.innerHTML="";
-    for(let i=0;i<data.length;i++){
-        mainDiv.innerHTML=  mainDiv.innerHTML+`
-        <div class="p-2 col-lg-4 col-sm-12">
-            <div class="card mx-auto" style="width: 18rem;">
-                <h5 class="card-title p-2 text-center bg-dark text-white mb-0">${data[i].name}</h5>
+displayRestCountries().then(data=>{
+    for(var i=0;i<data.length;i++){
+        //console.log(data[i].name);
+        row.innerHTML+=`
+        <div class="col-sm-6 col-md-4 col-lg-4 col-xl-4">
+            <div class="card h-100" >
+                <div class="card-header text-center bg-dark text-white">${data[i].name}</div>
                 <div class="card-body text-center" id="card-body">
                     <img src="${data[i].flags.svg}" class="card-img-top" alt="...">
-                    <p class="card-text">Capital: ${data[i].capital}</p>
-                    <p class="card-text">Region: ${data[i].region} </p>
-                    <p class="card-text">LatLng: ${data[i].latlng}</p>
-                    <p class="card-text">Country Code: ${data[i].alpha3Code}</Code></p>
+                    <div class="card-text">Region: ${data[i].region} <div>
+                    <div class="card-text">Capital: ${data[i].capital}</div>
+                    <div class="card-text">LatLng: ${data[i].latlng}</div>
+                    <div class="card-text">Country Code: ${data[i].alpha3Code}</Code></div>
                     <a href="#" class="btn btn-primary border justify-content-center" >Click for Weather</a>
                  </div>
             </div>
          </div>
-        `
+        `;
+        document.body.append(container);
     }
     return data;
 }).then(data=>{ //after the data is recieved then fetching the weather data based on rest country data
@@ -79,10 +53,15 @@ displayRestCountries()
         }).then(data=>{
             //displaying the weather data as a alert message
             alert(`
-                Weather
+                            WEATHER DATA 
+                ---------------------------------
                 Descritpion: ${data.weather[0].description}
                 Humidity: ${data.main.humidity}
                 Temperature: ${data.main.temp}
+                Minimum Temperature : ${data.main.temp_min}
+                Maximum Temperatur : ${data.main.temp_max}
+                Pressure: ${data.main.pressure}
+
             `);
         });
        });
